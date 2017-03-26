2af0c730ee27fb281dffb805f502f22cedd013df
乔福 <qiaofu@taobao.com>
2017-03-26 19:00:35 +0800

    add sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,31 @@
 // this is my sw.js
+var CACHE_NAME = 'v1';
+var urlsToCache = [
+  '/',
+  '/manifest.json',
+  '/js/theme.min.js',
+  '/css/main.css',
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  console.log(event.request);
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
